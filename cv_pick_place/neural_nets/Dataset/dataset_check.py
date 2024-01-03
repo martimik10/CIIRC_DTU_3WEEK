00505888bb312b8f7708f614bd7d2f758a493ffe
sakasa458@gmail.com <sakasa458@gmail.com>
2024-01-03 14:15:36 +0100

    chnged  structure of  files

---
+++ cv_pick_place/neural_nets/Dataset/dataset_check.py
@@ -0,0 +1,41 @@
+
+import os 
+import shutil
+
+
+DATASET_ROOT_PATH="./GeneratedDataset"
+
+
+
+def dataset_check(generate_dataset = True,generate_video = False) ->bool:
+    """
+    params:
+    generate_new :  to generate new dataset 
+    generate video : generates video for validation 
+    
+    """
+    if(generate_dataset):
+        if(os.path.isdir(DATASET_ROOT_PATH)):
+             print("Deleting old dataset...")
+        #remove the old dataset
+        for filename in os.listdir(DATASET_ROOT_PATH):
+            file_path = os.path.join(DATASET_ROOT_PATH, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+
+            except Exception as e:
+                print('Failed to delete %s. Reason: %s' % (file_path, e))
+                return False
+        return True
+    if(generate_video):
+        return True
+    else:
+        return False 
+
+    
+    
+
+   
---
+++ cv_pick_place/neural_nets/object_detect/train.py
@@ -0,0 +1,13 @@
+import sys
+import os 
+
+
+# sys.path.append('..')
+from ...Dataset import *
+# from Dataset.GenerateSyntheticDataset import generate_dataset
+
+
+
+
+if(dataset_check):
+    generate_dataset(custom_num_images=10,segmentation=False)
