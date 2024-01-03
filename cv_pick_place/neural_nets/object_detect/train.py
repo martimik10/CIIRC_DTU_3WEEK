af4bf49485421fc541c5657f934048f1c63cbef1
Martin Mikšík <martimik10@gmail.com>
2024-01-03 14:33:17 +0100

    gitignore changes

--- cv_pick_place/neural_nets/object_detect/train.py
+++ cv_pick_place/neural_nets/object_detect/train.py
@@ -1,13 +1,23 @@
+
+import os
 import sys
-import os 
+import inspect
 
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
 
+#get current directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+print("curr_dir", current_dir)
+print("parent_dir", parentdir)
 # sys.path.append('..')
-from ...Dataset import *
-# from Dataset.GenerateSyntheticDataset import generate_dataset
+# from ...Dataset import *
+from Dataset.GenerateSyntheticDataset import generate_dataset
 
 
 
+DATASET_CHECK = False
 
-if(dataset_check):
+if(DATASET_CHECK):
     generate_dataset(custom_num_images=10,segmentation=False)
