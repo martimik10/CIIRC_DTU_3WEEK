c2633b5b90499d04558050949ff7da39d8c8283e
HitchedLime <sakasa458@gmail.com>
2024-01-08 14:23:05 +0100

    Testing files for Mik

---
+++ testing/camer_net_testing.py
@@ -0,0 +1,141 @@
+import os
+import time 
+
+
+from ultralytics import YOLO
+import cv2
+from ultralytics.utils.plotting import Annotator
+from pathlib import Path
+
+from realsense_depth_test import DepthCamera
+
+
+camera = DepthCamera(config_path=Path("C:/Users/Testbed/CIIRC_DTU_3WEEK_NEW/cv_pick_place/config/D435_camera_config.json"))
+
+# Get frames from camera
+
+
+
+def run(frame,model):
+        
+        
+        ouput = model(frame)
+        
+        return ouput
+
+            
+
+if __name__ == "__main__":
+    # model_to_use=model_to_use = f'runs/detect/train/weights/best.pt'
+    model_to_use=model_to_use = f'C:\\Users\\Testbed\\CIIRC_DTU_3WEEK_NEW\\utils\\best_segment_08_01.pt'
+
+    try:
+        model = YOLO(model_to_use)
+        
+    except FileNotFoundError:
+        print("Model not found, YoloV8n")
+
+
+    img_array =[]
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    bottomLeftCornerOfText = (0, 315)
+    fontScale = 1
+    fontColor = (0, 0, 255)
+    thickness = 2
+    lineType = 2
+    prev_frame_time= 0
+    while True:
+        success,frame_timestamp,depth_frame,rgb_frame,colorized_depth = camera.get_frames()
+        if not success:
+            continue
+        img_frame = rgb_frame.copy()
+        img_frame = cv2.resize(img_frame,(640,320))
+        results = run(img_frame,model)
+        annotated_frame = results[0].plot()
+        new_frame_time = time.time()
+        fps = 1 / (new_frame_time - prev_frame_time)
+        prev_frame_time = new_frame_time
+        fps = str(int(fps))
+        cv2.putText(annotated_frame, 'FPS: ' + fps,
+                    bottomLeftCornerOfText,
+                    font,
+                    fontScale,
+                    fontColor,
+                    thickness,
+                    lineType)
+
+        # Display the annotated frame
+        cv2.imshow("YOLOv8 Inference", annotated_frame)
+        img_array.append(annotated_frame)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+        
+
+
+    #     img_frame = rgb_frame.copy()
+    #     img_frame = cv2.resize(img_frame,(640,320))
+    #     data= run(img_frame,model)
+    #     for r in data: 
+            
+    #         boxes = r.boxes
+    #         annotator = Annotator(img_frame)
+        
+    #         for box in boxes:
+                
+    #             b = box.xyxy[0]  # get box coordinates in (left, top, right, bottom) format
+    #             c = box.cls
+    #             annotator.box_label(b, model.names[int(c)])
+          
+    #     img = annotator.result()  
+    #     cv2.imshow('YOLO V8 Detection', img)     
+    #     if cv2.waitKey(1) & 0xFF == ord(' '):
+    #         break
+
+        
+    # cv2.destroyAllWindows()
+
+
+
+
+# success, frame = cap.read()
+
+#     if success:
+#         # Run YOLOv8 inference on the frame
+#         results = model(frame)
+
+#         # Visualize the results on the frame
+#         annotated_frame = results[0].plot()
+        
+#         # time when we finish processing for this frame 
+#         new_frame_time = time.time()
+
+#         # fps will be number of frame processed in given time frame 
+#         # since their will be the most time error of 0.001 second 
+#         # we will be subtracting it to get more accurate result 
+#         fps = 1 / (new_frame_time - prev_frame_time)
+#         prev_frame_time = new_frame_time
+
+#         # converting the fps into integer 
+#         fps = str(int(fps))
+
+#         # putting the FPS count on the frame 
+#         cv2.putText(annotated_frame, 'FPS: ' + fps,
+#                     bottomLeftCornerOfText,
+#                     font,
+#                     fontScale,
+#                     fontColor,
+#                     thickness,
+#                     lineType)
+
+#         # Display the annotated frame
+#         cv2.imshow("YOLOv8 Inference", annotated_frame)
+#         img_array.append(annotated_frame)
+
+#         # Break the loop if 'q' is pressed
+#         if cv2.waitKey(1) & 0xFF == ord("q"):
+#             break
+#     else:
+#         # Break the loop if the end of the video is reached
+#         break
---
+++ testing/realsense_depth_test.py
@@ -0,0 +1,189 @@
+import json
+
+import numpy as np
+import pyrealsense2 as rs
+
+
+class IntelConfig:
+    """
+    Class for loading json config file into depth camera.
+    """
+
+    def __init__(self):
+        """
+        IntelConfig object constructor.
+
+        Args:
+            config_path (str): Path to the config file.
+        """
+
+        self.DS5_product_ids = [
+            "0AD1",
+            "0AD2",
+            "0AD3",
+            "0AD4",
+            "0AD5",
+            "0AF6",
+            "0AFE",
+            "0AFF",
+            "0B00",
+            "0B01",
+            "0B03",
+            "0B07",
+            "0B3A",
+            "0B5C",
+        ]
+
+    def find_device_that_supports_advanced_mode(self) -> rs.device:
+        """
+        Searches devices connected to the PC for one compatible with advanced mode.
+
+        Returns:
+            rs.device: RealSense device which supports advanced mode.
+        """
+
+        ctx = rs.context()
+        devices = ctx.query_devices()
+        for dev in devices:
+            if (
+                dev.supports(rs.camera_info.product_id)
+                and dev.supports(rs.camera_info.name)
+                and str(dev.get_info(rs.camera_info.product_id)) in self.DS5_product_ids
+            ):
+                print(
+                    "[INFO] Found device that supports advanced mode:",
+                    dev.get_info(rs.camera_info.name),
+                )
+                return dev
+
+        raise Exception(
+            "[ERROR] No RealSense camera that supports advanced mode was found"
+        )
+
+    def load_config(self, config_path: str):
+        """
+        Loads json config file into the camera.
+
+        Args:
+            config_path (str): Path to the config file.
+        """
+
+        # Open camera in advanced mode
+        dev = self.find_device_that_supports_advanced_mode()
+        advnc_mode = rs.rs400_advanced_mode(dev)
+
+        # Read configuration JSON file as string and print it to console
+        # serialized_string = advnc_mode.serialize_json()
+        # print(serialized_string)
+
+        # Write configuration file to camera
+        with open(config_path) as file:
+            data = json.load(file)
+        json_string = str(data).replace("'", '"')
+        advnc_mode.load_json(json_string)
+        print("[INFO] Loaded RealSense camera config from file:", config_path)
+
+
+class DepthCamera:
+    """
+    Class for connecting to and reading images from Inteal RealSense depth camera.
+    """
+
+    def __init__(
+        self,
+        config_path: str = None,
+    ):
+        """
+        IntelConfig object constructor.
+
+        Args:
+            config_path (str): Path to the config file.
+        """
+
+        # Check if any RealSense camera is connected
+        ctx = rs.context()
+        devices = ctx.query_devices()
+        is_camera_connected = len(devices) >= 1
+
+        if not is_camera_connected:
+            raise Exception("[ERROR] No RealSense camera was found")
+
+        if config_path is not None:
+            # If config file path was given, load camera config from provided file
+            self.ic = IntelConfig()
+            self.ic.load_config(config_path)
+        self.pipeline = rs.pipeline()
+        self.config = rs.config()
+
+        # Setup RGB and Depth stream resolution, format and FPS
+        # Maximal supported Depth stream resolution of D435 camera is 1280 x 720
+        # Maximal supported RGB stream resolution of D435 camera is 1920 x 1080
+        self.config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 15)
+        self.config.enable_stream(rs.stream.color, 1920, 1080, rs.format.bgr8, 15)
+
+        # Create object for aligning depth frame to RGB frame, so that they have equal resolution
+        self.align = rs.align(rs.stream.color)
+
+        # Create object for filling missing depth pixels where the sensor was not able to detect depth
+        self.hole_filling = rs.hole_filling_filter()
+        self.hole_filling.set_option(rs.option.holes_fill, 2)
+
+        # Create object for colorizing depth frames
+        self.colorizer = rs.colorizer()
+        self.colorizer.set_option(rs.option.color_scheme, 0)
+
+        # Start video stream
+        self.profile = self.pipeline.start(self.config)
+
+        # Code for getting camera intrinsic parameters
+        # self.stream_profile_color = self.profile.get_stream(rs.stream.color, 0)
+        # self.stream_profile_depth = self.profile.get_stream(rs.stream.depth, 0)
+        # self.video_stream_profile = self.stream_profile_color.as_video_stream_profile()
+        # ; = self.video_stream_profile.get_intrinsics()
+        # print("fx =", intrinsics.fx, "; fy =", intrinsics.fy)
+        # print("ppx =", intrinsics.ppx, "; ppy =", intrinsics.ppy)
+        # print("height =", intrinsics.height, "; width =", intrinsics.width)
+        # print("coeffs =", intrinsics.coeffs)
+        # print("model =", intrinsics.model)
+
+    def get_frames(self) -> tuple[bool, np.ndarray, np.ndarray, np.ndarray]:
+        """
+        Reads and processes frames from connected camera.
+
+        Returns:
+            bool: True if frame was succesfully read
+            float: Time in milliseconds when the frame was captured
+            np.ndarray: RGB frame
+            np.ndarray: Depth frame
+            np.ndarray: Colorized depth frame
+        """
+
+        # Reads RGB and depth frames and resize them to same resolution
+        frameset = self.pipeline.wait_for_frames()
+        frameset = self.align.process(frameset)
+
+        # Extract RGB and depth frames from frameset
+        depth_frame = frameset.get_depth_frame()
+        color_frame = frameset.get_color_frame()
+        frame_timestamp = frameset.get_timestamp()
+
+        if not depth_frame or not color_frame:
+            return False, None, None, None
+
+        # Apply hole filling filter
+        depth_frame = self.hole_filling.process(depth_frame)
+
+        colorized_depth_frame = np.asanyarray(
+            self.colorizer.colorize(depth_frame).get_data()
+        )
+        depth_frame = np.asanyarray(depth_frame.get_data())
+        color_frame = np.asanyarray(color_frame.get_data())
+
+        return True, frame_timestamp, depth_frame, color_frame, colorized_depth_frame
+
+    def release(self):
+        """
+        Disconnects the camera.
+        """
+
+        self.pipeline.stop()
